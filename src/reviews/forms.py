35168a77e8b4bc382fd992aed57d8d3c040d3a9c
TRAN Tan <tan.tran.workpro@gmail.com>
2022-08-03 00:32:29 +0200

    - Clean the css codes. - Clean the python codes according to PEP8 convention.

--- src/reviews/forms.py
+++ src/reviews/forms.py
@@ -17,7 +17,9 @@ class ReviewForm(forms.ModelForm):
                    (4, '- 4'),
                    (5, '- 5')
                    ]
-        widgets = {"rating": forms.RadioSelect(choices=CHOICES)}
+        widgets = {
+            "rating": forms.RadioSelect(choices=CHOICES)
+        }
 
 
 class TicketForm(forms.ModelForm):
@@ -27,36 +29,3 @@ class TicketForm(forms.ModelForm):
         labels = {"title": "Titre",
                   "description": "Description",
                   "image": "Image"}
-
-# class TicketForm(forms.ModelForm):
-#     title = forms.CharField(
-#         label="Title",
-#         max_length=128,
-#         widget=forms.TextInput()
-#     )
-#     description = forms.CharField(
-#         label="Description",
-#         max_length=2048,
-#         widget=forms.Textarea(),
-#         required=False
-#     )
-#     image = forms.ImageField(
-#         label="Image",
-#         required=False
-#     )
-#
-#     class Meta:
-#         model = Ticket
-#         fields = ['title', 'description', 'image']
-
-# class UserRegisterForm(UserCreationForm):
-#     class Meta:
-#         model = User
-#         fields = ['username', 'password1', 'password2']
-#
-#
-# class SubscriptionForm(forms.Form):
-#     followed_user = forms.CharField(
-#         label=False,
-#         widget=forms.TextInput()
-#     )
--- src/reviews/views.py
+++ src/reviews/views.py
@@ -1,18 +1,60 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.datastructures import MultiValueDictKeyError
-
 from reviews.forms import ReviewForm, TicketForm
 from reviews.models import Ticket, Review
 from itertools import chain
-
 from django.db.models import CharField, Value
-
-from users.models import UserFollow
 from .utils_func import (
     get_users_viewable_reviews,
     get_users_viewable_tickets,
 )
+
+
+def flux(request):
+    tag = "flux"
+    if not request.user.is_authenticated:
+        return redirect('login')
+    reviews = get_users_viewable_reviews(request.user)
+
+    # returns queryset of reviews
+    reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
+    tickets = get_users_viewable_tickets(request.user)
+
+    # returns queryset of tickets
+    tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
+
+    # combine and sort the two types of posts
+    all_posts = sorted(
+        chain(reviews, tickets),
+        key=lambda post: post.time_created,
+        reverse=True
+    )
+    user_all_reviews = Review.objects.filter(user=request.user)
+    ticket_already_answered = [user_review.ticket_id for user_review in user_all_reviews]
+
+    return render(request, 'reviews/feed.html', context={'all_posts': all_posts,
+                                                         'tag': tag,
+                                                         'ticket_already_answered': ticket_already_answered
+                                                         })
+
+
+@login_required
+def posts(request):
+    tag = "posts"
+    user_all_tickets = (Ticket.objects.filter(user=request.user)).annotate(content_type=Value('TICKET', CharField()))
+    user_all_reviews = (Review.objects.filter(user=request.user)).annotate(content_type=Value('REVIEW', CharField()))
+
+    all_posts = sorted(
+        chain(user_all_reviews, user_all_tickets),
+        key=lambda post: post.time_created,
+        reverse=True
+    )
+
+    return render(request, 'reviews/feed.html', {'all_posts': all_posts,
+                                                 'tag': tag
+                                                 })
+
 
 @login_required
 def create_review_solo(request):
@@ -30,42 +72,41 @@ def create_review_solo(request):
                                                description=request.POST['description'],
                                                user=request.user,
                                                image=image)
-            new_review = Review.objects.create(ticket=new_ticket,
-                                               rating=request.POST['rating'],
-                                               user=request.user,
-                                               headline=request.POST['headline'],
-                                               body=request.POST['body'])
+            Review.objects.create(ticket=new_ticket,
+                                  rating=request.POST['rating'],
+                                  user=request.user,
+                                  headline=request.POST['headline'],
+                                  body=request.POST['body'])
             return redirect('flux')
     else:
         r_form = ReviewForm()
         t_form = TicketForm()
 
     return render(request, 'reviews/create_review.html', {'r_form': r_form,
-                                                               't_form': t_form,
-                                                               'tag': tag
-                                                               })
+                                                          't_form': t_form,
+                                                          'tag': tag
+                                                          })
 
 
 @login_required
-def create_review(request, pk):
-    ticket = get_object_or_404(Ticket, id=pk)
-    tag = "create_review"
+def create_ticket(request):
     if request.method == 'POST':
-        r_form = ReviewForm(request.POST)
-        if r_form.is_valid():
-            new_review = Review.objects.create(ticket=ticket,
-                                               rating=request.POST['rating'],
-                                               user=request.user,
-                                               headline=request.POST['headline'],
-                                               body=request.POST['body'])
+        t_form = TicketForm(request.POST, request.FILES)
+        if t_form.is_valid():
+            try:
+                image = request.FILES['image']
+            except MultiValueDictKeyError:
+                image = None
+
+            Ticket.objects.create(title=request.POST['title'],
+                                  description=request.POST['description'],
+                                  user=request.user,
+                                  image=image)
             return redirect('flux')
     else:
-        r_form = ReviewForm()
+        t_form = TicketForm()
 
-    return render(request, 'reviews/create_review.html', {'r_form': r_form,
-                                                          'post': ticket,
-                                                          'tag': tag
-                                                          })
+    return render(request, 'reviews/create_ticket.html', {'t_form': t_form})
 
 
 @login_required
@@ -83,31 +124,32 @@ def update_ticket(request, pk):
 
 
 @login_required
-def create_ticket(request):
-    if request.method == 'POST':
-        t_form = TicketForm(request.POST, request.FILES)
-        if t_form.is_valid():
-            try:
-                image = request.FILES['image']
-            except MultiValueDictKeyError:
-                image = None
-
-            new_ticket = Ticket.objects.create(title=request.POST['title'],
-                                               description=request.POST['description'],
-                                               user=request.user,
-                                               image=image)
-            return redirect('flux')
-    else:
-        t_form = TicketForm()
-
-    return render(request, 'reviews/create_ticket.html', {'t_form': t_form})
-
-
-@login_required
 def delete_ticket(request, pk):
     ticket = get_object_or_404(Ticket, id=pk)
     ticket.delete()
     return redirect('posts')
+
+
+@login_required
+def create_review(request, pk):
+    ticket = get_object_or_404(Ticket, id=pk)
+    tag = "create_review"
+    if request.method == 'POST':
+        r_form = ReviewForm(request.POST)
+        if r_form.is_valid():
+            Review.objects.create(ticket=ticket,
+                                  rating=request.POST['rating'],
+                                  user=request.user,
+                                  headline=request.POST['headline'],
+                                  body=request.POST['body'])
+            return redirect('flux')
+    else:
+        r_form = ReviewForm()
+
+    return render(request, 'reviews/create_review.html', {'r_form': r_form,
+                                                          'post': ticket,
+                                                          'tag': tag
+                                                          })
 
 
 @login_required
@@ -128,55 +170,6 @@ def update_review(request, pk):
 
 @login_required
 def delete_review(request, pk):
-    review = get_object_or_404(Review, id=pk)
+    review = Review.objects.get(id=pk)
     review.delete()
     return redirect('posts')
-
-@login_required
-def posts(request):
-    user_all_tickets = Ticket.objects.filter(user=request.user)
-    user_all_reviews = Review.objects.filter(user=request.user)
-
-    user_all_tickets = user_all_tickets.annotate(content_type=Value('TICKET', CharField()))
-    user_all_reviews = user_all_reviews.annotate(content_type=Value('REVIEW', CharField()))
-    tag = "posts"
-
-    all_posts = sorted(
-        chain(user_all_reviews, user_all_tickets),
-        key=lambda post: post.time_created,
-        reverse=True
-    )
-
-    return render(request, 'reviews/feed.html', {'all_posts': all_posts,
-                                                 'tag': tag
-                                                 })
-
-
-def flux(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
-    reviews = get_users_viewable_reviews(request.user)
-
-    # returns queryset of reviews
-    reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
-    tickets = get_users_viewable_tickets(request.user)
-
-    # returns queryset of reviews
-    user_all_reviews = Review.objects.filter(user=request.user)
-    ticket_already_answered = [user_review.ticket_id for user_review in user_all_reviews]
-
-    # returns queryset of tickets
-    tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
-
-    # combine and sort the two types of posts
-    all_posts = sorted(
-        chain(reviews, tickets),
-        key=lambda post: post.time_created,
-        reverse=True
-    )
-
-    tag = "flux"
-    return render(request, 'reviews/feed.html', context={'all_posts': all_posts,
-                                                         'tag': tag,
-                                                         'ticket_already_answered': ticket_already_answered
-                                                         })
